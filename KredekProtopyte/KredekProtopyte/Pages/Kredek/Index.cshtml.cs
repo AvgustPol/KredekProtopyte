880379bafda7f64618fda6432b3c810c903a3864
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-02-24 23:28:49 +0100

    Initial commit

---
+++ KredekProtopyte/KredekProtopyte/Pages/Kredek/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KredekProtopyte.Pages.Kredek
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KredekProtopyte/KredekProtopyte/Pages/Kredek/Team.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KredekProtopyte.Pages.Kredek
+{
+    public class TeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
