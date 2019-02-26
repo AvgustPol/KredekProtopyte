6d2a4ed1f1b964e4768004e0c95a0ea7bf3d640c
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-02-26 21:18:58 +0100

    added banner template partial
    note: template is not finished yet.

---
+++ KredekProtopyte/KredekProtopyte/Data/BannerTemplate.cs
@@ -0,0 +1,9 @@
+﻿namespace KredekProtopyte.Data
+{
+    public class BannerTemplate
+    {
+        public string ImageUrl { get; set; }
+        public string SubTitle { get; set; }
+        public string Title { get; set; }
+    }
+}
--- KredekProtopyte/KredekProtopyte/Pages/Kredek/Index.cshtml.cs
+++ KredekProtopyte/KredekProtopyte/Pages/Kredek/Index.cshtml.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using KredekProtopyte.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace KredekProtopyte.Pages.Kredek
 {
     public class IndexModel : PageModel
     {
+        public BannerTemplate BannerTemplate { get; set; }
+
         public void OnGet()
         {
+            BannerTemplate = new BannerTemplate()
+            {
+                ImageUrl = "https://images.wallpaperscraft.com/image/desert_sand_sky_136050_1920x1080.jpg",
+                SubTitle = "Creation and Development Group",
+                Title = "Kredek"
+            };
         }
     }
 }
