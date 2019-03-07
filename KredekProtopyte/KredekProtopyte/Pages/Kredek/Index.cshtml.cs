a22f80713d9c85ec0b6b9b9c315010be92cabf2c
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-03-07 11:09:02 +0100

    Update Index.cshtml.cs
    Adde CopyFolderContents() Method

--- KredekProtopyte/KredekProtopyte/Pages/Kredek/Index.cshtml.cs
+++ KredekProtopyte/KredekProtopyte/Pages/Kredek/Index.cshtml.cs
@@ -1,8 +1,8 @@
 ﻿using KredekProtopyte.Data;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Diagnostics;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace KredekProtopyte.Pages.Kredek
 {
@@ -11,34 +11,28 @@ namespace KredekProtopyte.Pages.Kredek
         public PageContent PageContent { get; set; }
         public string PageName { get; set; } = "MyNewTestPage";
 
-        public async Task<IActionResult> CreateNewRazorPage()
+        public void CreateNewRazorPage()
         {
             //TODO why it is not working? It doesnt create a new file. Maybe this code is for copying only directories .
-            string SourcePath = @"D:\Programming\CMS\KredekProtopyte\KredekProtopyte\KredekProtopyte\Pages\Kredek\Index.cshtml.cs";
-            string DestinationPath = $@"D:\Programming\CMS\KredekProtopyte\KredekProtopyte\KredekProtopyte\Pages\Kredek\{PageName}.cshtml.cs";
+            string SourcePath = @"D:\Programming\CMS\KredekProtopyte\KredekProtopyte\KredekProtopyte\Pages\Template";
+            string DestinationPath = $@"D:\Programming\CMS\KredekProtopyte\KredekProtopyte\KredekProtopyte\Pages\{PageName}";
 
-            //Now Create all of the directories
-            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",
-                SearchOption.AllDirectories))
-                Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+            CopyFolderContents(SourcePath, DestinationPath);
 
-            //Copy all the files & Replaces any files with the same name
-            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
-                SearchOption.AllDirectories))
-                System.IO.File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+            ////Now Create all of the directories
+            //foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",
+            //    SearchOption.AllDirectories))
+            //    Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
 
-            return null;
+            ////Copy all the files & Replaces any files with the same name
+            //foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
+            //    SearchOption.AllDirectories))
+            //    System.IO.File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
         }
-
-        //public async Task<IActionResult> CreateNewRazorPage()
-        //{
-        //    CreateNewRazorPage(PageName, );
-        //    CreateNewRazorPage(PageName, );
-        //}
 
         public void OnGet()
         {
-            CreateNewRazorPage();
+            //CreateNewRazorPage();
 
             // TODO : add DbContext And Users registration, facebook authentication, etc.
             PageContent = new PageContent();
@@ -65,6 +59,47 @@ namespace KredekProtopyte.Pages.Kredek
                 Title = "A tutaj nie bedzie krotki opis naszego kola",
                 Text = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
             });
+        }
+
+        private bool CopyFolderContents(string SourcePath, string DestinationPath)
+        {
+            SourcePath = SourcePath.EndsWith(@"\") ? SourcePath : SourcePath + @"\";
+            DestinationPath = DestinationPath.EndsWith(@"\") ? DestinationPath : DestinationPath + @"\";
+
+            try
+            {
+                if (Directory.Exists(SourcePath))
+                {
+                    if (Directory.Exists(DestinationPath) == false)
+                    {
+                        Directory.CreateDirectory(DestinationPath);
+                    }
+
+                    foreach (string files in Directory.GetFiles(SourcePath))
+                    {
+                        FileInfo fileInfo = new FileInfo(files);
+                        fileInfo.CopyTo(string.Format(@"{0}\{1}", DestinationPath, fileInfo.Name), true);
+                    }
+
+                    foreach (string drs in Directory.GetDirectories(SourcePath))
+                    {
+                        DirectoryInfo directoryInfo = new DirectoryInfo(drs);
+                        if (CopyFolderContents(drs, DestinationPath + directoryInfo.Name) == false)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("");
+                Debug.WriteLine($"Error: {ex.Message}");
+                Debug.WriteLine("");
+
+                return false;
+            }
         }
     }
 }
